72d5215c4088884499131f2ea23eb2a32a59e4be
kiwidiwi <asfa42886@gmail.com>
2024-08-24 16:52:35 +0200

    Fixed error

--- bump_version.sh
+++ bump_version.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Read the current version from version.json
-current_version=$(grep -oP '"version": "\K[^"]+' version.json)
+current_version=$(grep -oE '"version": "([^"]+)"' version.json | sed -E 's/"version": "([^"]+)"/\1/')
 
 # Increment the version number
 new_version=$((current_version + 1))
 
 # Update version.json with the new version number
-sed -i "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" version.json
+#sed -i "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" version.json
+sed "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" version.json > temp.json && mv temp.json version.json
+
 
 # Commit and push the changes to GitHub
 git add version.json
